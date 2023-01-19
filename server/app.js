405fb64dd638b955388e8e240f6a7acdabd49554
Kellen Bavis <kellenbavis10@gmail.com>
2023-01-18 19:04:06 -0500

    small update to app.js

--- server/app.js
+++ server/app.js
@@ -44,7 +44,6 @@ db.once("open", function () {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
